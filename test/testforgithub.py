6d4a9f58f2f0a653cca379c7a7a8482e9250110d
turjay <tturkaykurtaran@gmail.com>
2024-09-11 14:04:37 +0300

    Fixed github test file

--- test/testforgithub.py
+++ test/testforgithub.py
@@ -7,9 +7,113 @@ from tqdm import tqdm
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-# Import additional signals
-from indicators import stochastic_trade_signal, atr_trade_signal
+from main import train_model, model_trade_signal, SignalPool
+from indicators import bollinger_trade_signal, macd_trade_signal, rsi_trade_signal, stochastic_trade_signal, atr_trade_signal
 from config import logger
+
+def get_yahoo_data():
+    """
+    Yahoo Finance'ten BTC-USD verisini indirir ve gerekli sütunları seçer.
+    Returns:
+        pd.DataFrame: İçinde Datetime ve close sütunları bulunan veri seti.
+    """
+    symbol = 'BTC-USD'
+    data = yf.download(symbol, period='3mo', interval='1h')
+    data.reset_index(inplace=True)
+    data.rename(columns={'Close': 'close'}, inplace=True)
+    return data[['Datetime', 'close']]
+
+def run_backtest(indicator_signal, indicator_name, initial_balance_usd=50):
+    """
+    Belirli bir indikatör sinyali kullanarak geriye dönük test gerçekleştirir.
+    """
+    data = get_yahoo_data()
+    balance_usd = initial_balance_usd
+    btc_balance = 0
+    balance_history = []
+    trade_log = pd.DataFrame(columns=['Datetime', 'Action', 'Price', 'BTC_Balance', 'USD_Balance'])
+
+    for i in tqdm(range(20, len(data)), desc=f"Running {indicator_name} Backtest"):
+        df = data.iloc[:i].copy()
+        signal = indicator_signal(df)
+        
+        close_price = df['close'].iloc[-1]
+        datetime = df['Datetime'].iloc[-1]
+        
+        if signal == 'buy' and balance_usd > 0:
+            btc_balance = balance_usd / close_price
+            balance_usd = 0
+            new_row = pd.DataFrame({
+                'Datetime': [datetime],
+                'Action': ['buy'],
+                'Price': [close_price],
+                'BTC_Balance': [btc_balance],
+                'USD_Balance': [balance_usd]
+            })
+            trade_log = pd.concat([trade_log, new_row], ignore_index=True)
+        elif signal == 'sell' and btc_balance > 0:
+            balance_usd = btc_balance * close_price
+            btc_balance = 0
+            new_row = pd.DataFrame({
+                'Datetime': [datetime],
+                'Action': ['sell'],
+                'Price': [close_price],
+                'BTC_Balance': [btc_balance],
+                'USD_Balance': [balance_usd]
+            })
+            trade_log = pd.concat([trade_log, new_row], ignore_index=True)
+        
+        total_balance = balance_usd + btc_balance * close_price
+        balance_history.append(total_balance)
+
+    return balance_history, trade_log
+
+def run_ml_backtest(initial_balance_usd=50):
+    """
+    Makine öğrenmesi modeli kullanarak geriye dönük test gerçekleştirir.
+    """
+    data = get_yahoo_data()
+    balance_usd = initial_balance_usd
+    btc_balance = 0
+    balance_history = []
+    trade_log = pd.DataFrame(columns=['Datetime', 'Action', 'Price', 'BTC_Balance', 'USD_Balance'])
+
+    model = train_model(data)
+
+    for i in tqdm(range(20, len(data)), desc="Running ML Backtest"):
+        df = data.iloc[:i].copy()
+        signal = model_trade_signal(df, model)
+
+        close_price = df['close'].iloc[-1]
+        datetime = df['Datetime'].iloc[-1]
+
+        if signal == 'buy' and balance_usd > 0:
+            btc_balance = balance_usd / close_price
+            balance_usd = 0
+            new_row = pd.DataFrame({
+                'Datetime': [datetime],
+                'Action': ['buy'],
+                'Price': [close_price],
+                'BTC_Balance': [btc_balance],
+                'USD_Balance': [balance_usd]
+            })
+            trade_log = pd.concat([trade_log, new_row], ignore_index=True)
+        elif signal == 'sell' and btc_balance > 0:
+            balance_usd = btc_balance * close_price
+            btc_balance = 0
+            new_row = pd.DataFrame({
+                'Datetime': [datetime],
+                'Action': ['sell'],
+                'Price': [close_price],
+                'BTC_Balance': [btc_balance],
+                'USD_Balance': [balance_usd]
+            })
+            trade_log = pd.concat([trade_log, new_row], ignore_index=True)
+
+        total_balance = balance_usd + btc_balance * close_price
+        balance_history.append(total_balance)
+
+    return balance_history, trade_log
 
 def run_backtests():
     logger.info("Running backtests with various indicators...")
@@ -44,6 +148,37 @@ def run_backtests():
     plt.legend()
     plt.grid(True)
     plt.show()
+
+# Test fonksiyonları
+def test_bollinger_backtest():
+    balance_history, _ = run_backtest(bollinger_trade_signal, 'Bollinger Bands')
+    assert len(balance_history) > 0
+    assert balance_history[-1] > 0
+
+def test_macd_backtest():
+    balance_history, _ = run_backtest(macd_trade_signal, 'MACD')
+    assert len(balance_history) > 0
+    assert balance_history[-1] > 0
+
+def test_rsi_backtest():
+    balance_history, _ = run_backtest(rsi_trade_signal, 'RSI')
+    assert len(balance_history) > 0
+    assert balance_history[-1] > 0
+
+def test_stochastic_backtest():
+    balance_history, _ = run_backtest(stochastic_trade_signal, 'Stochastic Oscillator')
+    assert len(balance_history) > 0
+    assert balance_history[-1] > 0
+
+def test_atr_backtest():
+    balance_history, _ = run_backtest(atr_trade_signal, 'ATR')
+    assert len(balance_history) > 0
+    assert balance_history[-1] > 0
+
+def test_ml_backtest():
+    balance_history, _ = run_ml_backtest()
+    assert len(balance_history) > 0
+    assert balance_history[-1] > 0
 
 if __name__ == "__main__":
     run_backtests()
